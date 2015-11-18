2bfdd5a777f41223b109f7da39e6cebb2128dcef
Scott Greene <zarthrogo@gmail.com>
2015-11-18 14:25:46 -0800

    This is my second change to index.php

--- test.php
+++ test.php
@@ -4,9 +4,9 @@
 
 <body>
 <h1> Imagine this is a cool page!</h1>
-<p>Version 2</p>
+<p>Version 3</p>
+<p>Different Clever content goes here!</p>
 <p>Clever content goes here!</p>
-<p>Clever content goes here!</p>
-<p>Clever content goes here!</p>
+<p>This is also different Clever content goes here!</p>
 </body>
 </html>
