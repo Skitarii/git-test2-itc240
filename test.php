d1ec1f63658ec190c5fb2e9b73259f599b6fcfbe
Scott Greene <zarthrogo@gmail.com>
2015-11-18 14:12:47 -0800

    Initial test page made

---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+
+<body>
+<h1> Imagine this is a cool page!</h1>
+<p>Version 2</p>
+<p>Clever content goes here!</p>
+<p>Clever content goes here!</p>
+<p>Clever content goes here!</p>
+</body>
+</html>
